f5ad0c6667f3d80788cc5bd42beaf3628869d294
byoungjupark <byoungju1012@gmail.com>
2021-10-23 12:40:53 +0900

    Set Paginations

--- ebooks/api/views.py
+++ ebooks/api/views.py
@@ -1,17 +1,18 @@
-from rest_framework            import generics
+from rest_framework            import generics, mixins, pagination
 from rest_framework            import permissions
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics   import get_object_or_404
 
 from ebooks.models          import Ebook, Review
+from ebooks.api.pagination  import SmallSetPagination
 from ebooks.api.serializers import EbookSerializer, ReviewSerializer
 from ebooks.api.permissions import IsAdminUserOrReadOnly, IsReviewAuthorOrReadOnly
 
 class EbookListCreateAPIView(generics.ListCreateAPIView):
-    queryset           = Ebook.objects.all()
+    queryset           = Ebook.objects.all().order_by("-id")
     serializer_class   = EbookSerializer
     permission_classes = [IsAdminUserOrReadOnly]
-
+    pagination_class   = SmallSetPagination
 
 class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset           = Ebook.objects.all()
@@ -45,13 +46,13 @@ class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
 
 
-# class EbookListCreateAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class EbookListCreateAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     
-#     queryset         = Ebook.objects.all()
-#     serializer_class = EbookSerializer
+    queryset         = Ebook.objects.all()
+    serializer_class = EbookSerializer
 
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
--- ebooksapi/settings.py
+++ ebooksapi/settings.py
@@ -127,3 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
 #     ]
 # }
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+#     'PAGE_SIZE': 3
+# }
